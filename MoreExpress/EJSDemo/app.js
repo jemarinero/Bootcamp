64d484bc7c07c114a9018506e9b634eee6e6c7fe
Josue Marinero <josue.marinero@gmail.com>
2017-10-28 11:21:17 -0600

    20171028

--- MoreExpress/EJSDemo/app.js
+++ MoreExpress/EJSDemo/app.js
@@ -1,13 +1,26 @@
 var express = require("express");
 var app = express();
 
+app.use(express.static("public"));
+app.set("view engine","ejs");
+
 app.get("/",function(req,res){
-    res.render("home.ejs");
+    res.render("home");
 });
 
 app.get("/fallinlovewith/:thing",function(req,res){
     var thing = req.params.thing;
-    res.render("love.ejs",{thingVar: thing});
+    res.render("love",{thingVar: thing});
+});
+
+app.get("/posts",function(req,res){
+    var posts = [
+        {title: "Post 1", author: "Susy"},
+        {title: "My adorable pet bunny", author: "Charly"},
+        {title: "Can you beleive this pomsky?", author: "Colt"}
+    ]
+
+    res.render("post",{posts: posts});
 });
 
 app.listen(3000,"localhost",function() {
